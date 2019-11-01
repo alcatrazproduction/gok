d24ea05416161be998f5455efa5927aa04862688
yves Huguenin <>
2019-11-01 20:23:37 +0100

    ***

--- gui/tranlation/de_de.ts
+++ gui/tranlation/de_de.ts
@@ -1,137 +1,110 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de" sourcelanguage="fr">
+<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="fr">
 <context>
     <name>mainWindow</name>
     <message>
-        <location filename="main.ui" line="18"/>
-        <source>KSW_Gok</source>
-        <translation>KSW_Gok</translation>
-    </message>
-    <message>
-        <location filename="main.ui" line="44"/>
-        <source>Citerne 1</source>
-        <translation>Tank</translation>
-    </message>
-    <message>
-        <location filename="main.ui" line="82"/>
-        <source>Ponrcentage de remplissage:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.ui" line="102"/>
+        <location filename="../../dispatcher.py" line="225"/>
         <source>Litrage:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="112"/>
+        <location filename="../../dispatcher.py" line="226"/>
         <source>Citerne / No de serie:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="122"/>
-        <source>Désignation de la citerne:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.ui" line="132"/>
-        <source>Unité:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="main.ui" line="139"/>
-        <location filename="main.ui" line="143"/>
+        <location filename="../../dispatcher.py" line="237"/>
         <source>Litres</source>
         <translation>Liter</translation>
     </message>
     <message>
-        <location filename="main.ui" line="148"/>
+        <location filename="../../dispatcher.py" line="238"/>
         <source>Metre Cube</source>
         <translation>Kubikmeter</translation>
     </message>
     <message>
-        <location filename="main.ui" line="153"/>
+        <location filename="../../dispatcher.py" line="239"/>
         <source>Pourcent</source>
         <translation>Prozent</translation>
     </message>
     <message>
-        <location filename="main.ui" line="158"/>
+        <location filename="../../dispatcher.py" line="240"/>
         <source>Hauteur(m)</source>
         <translation>Meter</translation>
     </message>
     <message>
-        <location filename="main.ui" line="163"/>
+        <location filename="../../dispatcher.py" line="241"/>
         <source>Kilogramme</source>
         <translation>Kilogramm</translation>
     </message>
     <message>
-        <location filename="main.ui" line="168"/>
+        <location filename="../../dispatcher.py" line="242"/>
         <source>IG</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="173"/>
+        <location filename="../../dispatcher.py" line="243"/>
         <source>UG</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="178"/>
+        <location filename="../../dispatcher.py" line="244"/>
         <source>Tonne</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="183"/>
+        <location filename="../../dispatcher.py" line="245"/>
         <source>milliBar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="188"/>
+        <location filename="../../dispatcher.py" line="246"/>
         <source>kiloPascal</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="196"/>
+        <location filename="../../dispatcher.py" line="229"/>
         <source>Type de citerne:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="206"/>
+        <location filename="../../dispatcher.py" line="230"/>
         <source>Volume de la citerne:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="216"/>
+        <location filename="../../dispatcher.py" line="231"/>
         <source>Hauteur de la citerne</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="226"/>
+        <location filename="../../dispatcher.py" line="232"/>
         <source>Temperature:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="236"/>
+        <location filename="../../dispatcher.py" line="233"/>
         <source>Date / Heure de la mesure:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="255"/>
+        <location filename="../../dispatcher.py" line="234"/>
         <source>dd.MM.yyyy HH:mm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="main.ui" line="323"/>
-        <source>Tab 2</source>
+        <location filename="../../dispatcher.py" line="224"/>
+        <source>Pourcentage de remplissage:</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <location filename="main.ui" line="339"/>
-        <source>Fic&amp;hier</source>
+    <message encoding="UTF-8">
+        <location filename="../../dispatcher.py" line="227"/>
+        <source>Désignation de la citerne:</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <location filename="main.ui" line="348"/>
-        <source>&amp;Quitter</source>
+    <message encoding="UTF-8">
+        <location filename="../../dispatcher.py" line="228"/>
+        <source>Unité:</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
