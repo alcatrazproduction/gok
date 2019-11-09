6b5442175e7def6b47186c27651e7d5b2aa451b4
yves Huguenin <>
2019-11-09 18:37:00 +0100

    ***

--- modules/sqlite3.py
+++ modules/sqlite3.py
@@ -5,4 +5,5 @@ import sqlite3
 class link:
 	def __init__(self, theHost=None, theDb=None,  theUser=None,  thePw=None):
 		self.con	= sqlite3.connect('db/gok.db')
+		self.con.row_factory = sqlite3.Row
 			
