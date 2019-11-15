0988e679249d9f918e33287cb3988fba9154e6c9
yves Huguenin <>
2019-11-15 22:43:34 +0100

    ***

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 #####################################################################################################
-#	Main application entry point																																										#
-#	Only import the main loop class and call all the init stuff, the run the application																											#
-#	Creator:		Yves Huguenin																																									#
-#	Date:			28.10.2019																																										#
-#	Version:		0.1																																													#
-#																																																		#
+#	Main application entry point
+#	Only import the main loop class and call all the init stuff, the run the application
+#	Creator:		Yves Huguenin
+#	Date:			28.10.2019
+#	Version:		0.1
+#	
 #####################################################################################################
 name = "KSWGok"
 
